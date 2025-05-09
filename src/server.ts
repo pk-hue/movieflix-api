d3d911494d8f62f69a5ad570e0336927976e6d15
Pedro H <ph159.com@gmail.com>
2025-05-09 00:03:52 -0300

    adicionada a rota de listagem de filmes

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
 import express from "express";
+import { PrismaClient } from './generated/prisma' 
+
 const port = 3000;
 const app = express();
+const prisma = new PrismaClient();
 
-app.get("/movies", (req, res) => {
-    res.send("listagem de filmes")
+app.get("/movies", async (req, res) => {
+    const movies = await prisma.movie.findMany();
+    res.json(movies);
 });
 
 app.listen(port, () => {
